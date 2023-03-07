4a9b5ad08344b7418d85a8d72f932b504f4197a6
sivapucc <124483364+sivapucc@users.noreply.github.com>
2023-03-06 16:42:38 -0800

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,14 @@
+var my_resume={
+    "name":"Siva Kumar",
+    "Age":"27",
+    "D.O.B":"09-12-1995",
+    "Email":"sivapucc@gmail.com",
+    "Mobile":"8608654408",
+    "Education":{
+        "Bca":"70%",
+        "HSC":"69.65%",
+        "SSLC":"85.5%"
+    },
+    "Programming_Language":"c,c++,java",
+
+};
